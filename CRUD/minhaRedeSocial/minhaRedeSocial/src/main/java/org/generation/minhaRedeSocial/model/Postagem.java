6e07b3ef51e11a3a7db61beba140dced469ecfce
unknown <samfir31@gmail.com>
2020-09-15 15:18:55 -0300

    .

---
+++ CRUD/minhaRedeSocial/minhaRedeSocial/src/main/java/org/generation/minhaRedeSocial/model/Postagem.java
@@ -0,0 +1,48 @@
+package org.generation.minhaRedeSocial.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table (name = "postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String descricao;	
+	
+	@NotNull
+	private int lik;	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getLik() {
+		return lik;
+	}
+	public void setLik(int lik) {
+		this.lik = lik;
+	}
+	
+	}
+	
+	
